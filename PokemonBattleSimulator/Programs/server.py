96419d167a5f37267bb1f2cd06d2c86c255e7b1d
DylanWalton <78657745+DylanWalton@users.noreply.github.com>
2023-01-02 15:29:25 +0100

    Update server.py

--- PokemonBattleSimulator/Programs/server.py
+++ PokemonBattleSimulator/Programs/server.py
@@ -1,7 +1,7 @@
 # Chat Room Connection - Client-To-Client
 import threading
 import socket
-host = "127.0.0.1"
+host = "172.20.10.9"
 port = 8080
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
@@ -11,20 +11,26 @@ aliases = []
 FORMAT = "utf-8"
 
 def broadcast(message) :
-    for client in clients :
-        client.send(message)
+    if message != "b''" :
+        print(message)
+        for client in clients :
+            client.send(message)
 
 def handle_client(client) :
     while True :
         try :
             message = client.recv(1024)
+            #print(message)
             broadcast(message)
         except :
             index = clients.index(client)
             clients.remove(client)
             client.close()
             alias = aliases[index]
-            broadcast(f"{alias} has left the chat room!\n".encode(FORMAT))
+            #broadcast(f"!playerleft:{alias}".encode(FORMAT))
+            msg = f"!left:{alias}"
+            broadcast(msg.encode(FORMAT))
+            #print(msg)
             aliases.remove(alias)
             break
 
@@ -41,6 +47,7 @@ def receive() :
         print(f"The alias of this client is {alias}".encode(FORMAT))
         broadcast(f"{alias} has connected to the chat room".encode(FORMAT))
         broadcast(f"!players:{aliases}".encode(FORMAT))
+        #print(aliases)
         client.recv(1024)
         client.send("You are now connected!".encode(FORMAT))
         thread = threading.Thread(target=handle_client, args=(client,))
